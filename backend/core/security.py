a63ef16dd151d9fb703f6b8a88ffdee5ea364f51
irtaza-khan <muhammadirtazakhan2003@gmail.com>
2026-06-30 12:17:07 +0500

    Development Ongoing

--- backend/core/security.py
+++ backend/core/security.py
@@ -27,10 +27,11 @@ def get_keycloak_admin() -> KeycloakAdmin:
             server_url=KEYCLOAK_URL,
             username=settings.KEYCLOAK_ADMIN_USER,
             password=settings.KEYCLOAK_ADMIN_PASSWORD,
-            realm_name=KEYCLOAK_REALM,
+            realm_name="master",
             user_realm_name="master",
             verify=True
         )
+        _keycloak_admin.connection.realm_name = KEYCLOAK_REALM
     return _keycloak_admin
 
 class TokenUser(BaseModel):
